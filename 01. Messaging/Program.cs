23067e5d8a9ad713773114e412b1a2f7bbb6c074
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-14 16:28:35 +0200

    Add project files.

---
+++ 01. Messaging/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+        foreach (var n in Console.ReadLine().Split())
+        {
+            numbers.Add(int.Parse(n));
+        }
+
+        List<char> text = new List<char>(Console.ReadLine().ToCharArray());
+        string result = "";
+
+        foreach (int num in numbers)
+        {
+            int sum = SumDigits(num);
+
+            int index = sum % text.Count;
+
+            result += text[index];
+
+            text.RemoveAt(index);
+        }
+
+        Console.WriteLine(result);
+    }
+
+    static int SumDigits(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
