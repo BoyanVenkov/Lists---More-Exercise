f19bbfc8f51af589672e3f26a28fa4fa5cdd2ce5
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-17 17:08:09 +0200

    Added a New Project

---
+++ 04. Mixed up Lists/Program.cs
@@ -0,0 +1,33 @@
+﻿List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> mixedList = new List<int>();
+
+int minCount = Math.Min(firstList.Count, secondList.Count);
+for (int i = 0; i < minCount; i++)
+{
+    mixedList.Add(firstList[i]);
+    mixedList.Add(secondList[secondList.Count - 1 - i]);
+}
+
+int lowRange = 0;
+int highRange = 0;
+
+if (firstList.Count > secondList.Count)
+{
+    int remaining1 = firstList[firstList.Count - 2];
+    int remaining2 = firstList[firstList.Count - 1];
+    highRange = Math.Max(remaining1, remaining2);
+    lowRange = Math.Min(remaining1, remaining2);
+}
+else
+{
+    int remaining1 = secondList[0];
+    int remaining2 = secondList[1];
+    highRange = Math.Max(remaining1, remaining2);
+    lowRange = Math.Min(remaining1, remaining2);
+}
+
+List<int> result = mixedList.Where(x => x > lowRange && x < highRange).ToList();
+result.Sort();
+
+Console.WriteLine(string.Join(" ", result));
